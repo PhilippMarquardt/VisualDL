cf0a61c8e69749ca6b7d11deec6b67e906757717
PhilippMarquardt <philipp.marquardt@ewetel.net>
2021-06-01 23:48:49 +0200

    Add basic segmentation model

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ from src.utils.utils import *
 from src.models.classification_model import *
 from src.trainer.classification.classification_trainer import *
 import logging
+
+
+from src.models.segmentation_model import *
+
+
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().name = ""
 
 #main()
 #def main():
-t = ClassificationTrainer(r"E:\source\repos\VisualDL\src\trainer\classification\classification.yaml")
-t.train()
+#t = ClassificationTrainer(r"E:\source\repos\VisualDL\src\trainer\classification\classification.yaml")
+#t.train()
 #t.get_visualization()
-di = t.test()
-print(di)
+#di = t.test()
+#print(di)
+t = SegmentationModel("resnet34", "Unet", 3, 3, None, None, None, None, None, None, None)
--- src/models/segmentation_model.py
+++ src/models/segmentation_model.py
@@ -1,6 +1,40 @@
 from src.models.model_base import ModelBase
-from ..dependencies import segmentation_modelspytorch
+import segmentation_models_pytorch as smp
+from torch.utils.data.dataloader import DataLoader
+from torch.nn import *
+from torch.optim import *
+from torch.utils.tensorboard import SummaryWriter
 
 class SegmentationModel(ModelBase):
-    def __init__(self, encoder_name,decoder_name ,nc, criterions, metrics, montitor_metric, optimizer, lr, accumulate_batch, tensorboard_dir):
+    def __init__(self, encoder_name, decoder_name,nc, in_channels, criterions, metrics, montitor_metric, optimizer, lr, accumulate_batch, tensorboard_dir):
+        self.encoder_name = encoder_name
+        self.decoder_name = decoder_name
+        self.model = eval(f'smp.{decoder_name}(encoder_name="{encoder_name}", encoder_weights="imagenet", in_channels={in_channels}, classes = {nc})')
+
+        self.writer = SummaryWriter(tensorboard_dir)
+        self.optimizer = eval(f"{optimizer}(self.model.parameters(), lr={lr})")
+        self.transform = None #TODO: Read transform from config
+        self.criterions = [eval(name)() for name in criterions]
+        self.metrics = metrics
+        self.monitor_metric = montitor_metric
+        self.accumulate_batch = accumulate_batch
+
+    def __call__(self, x):
+        return self.model(x)
+
+    def train(self, train_loader:DataLoader, valid_loader:DataLoader = None, test_loader:DataLoader = None, epochs:int = 1):
+        """Trains the model on the given DataLoader.
+
+        Args:
+            train_loader (DataLoader): The training DataLoader
+            valid_loader (DataLoader, optional): The valid DataLoader. Defaults to None.
+            test_loader (DataLoader, optional): The test DataLoader. Defaults to None.
+            epochs (int, optional): The number of epochs. Defaults to 1.
+        """
+        pass
+
+    def test(self, test_loader):
+        pass
         
+    def visualize(self, image):
+        pass
