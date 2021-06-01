e3c1d0b460a2c6da53591b54e675713a9d5d63e9
PhilippMarquardt <philipp.marquardt@ewetel.net>
2021-06-01 23:36:35 +0200

    Remove submodules

--- src/models/segmentation_model.py
+++ src/models/segmentation_model.py
@@ -1,6 +1,6 @@
 from src.models.model_base import ModelBase
-
+from ..dependencies import segmentation_modelspytorch
 
 class SegmentationModel(ModelBase):
-    def __init__(self):
-        pass
+    def __init__(self, encoder_name,decoder_name ,nc, criterions, metrics, montitor_metric, optimizer, lr, accumulate_batch, tensorboard_dir):
+        
