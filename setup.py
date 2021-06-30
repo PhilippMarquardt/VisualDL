734320fde66ac29ed96b4889d6d02d030f576be3
PhilippMarquardt <philipp.marquardt@ewetel.net>
2021-06-30 12:22:49 +0200

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
+import pkg_resources
+import pathlib
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
   name = 'visualdl',
   packages = find_packages(),
-  version = '0.0.1',
+  version = '0.0.2',
+  install_requires=install_requires,
   license='MIT',
   description = 'VisualDL - pytorch',
   author = 'Philipp Marquardt',
