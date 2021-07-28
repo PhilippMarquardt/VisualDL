fda91e9ab36cd938c3e2b98d3fe254400c9bc2ca
PhilippMarquardt <p.marquardt15@googlemail.com>
2021-07-28 11:39:55 +0200

    .

--- visualdl/inference/inference.py
+++ visualdl/inference/inference.py
@@ -4,13 +4,18 @@ from ..utils.model_utils import predict_images
 import torch
 
 class ModelInference():
-    def __init__(self, weight_path):
-        state = load(weight_path, map_location=torch.device('cpu'))
+    def __init__(self, weight_path, device = 'cuda:0' if torch.cuda.is_available() else 'cpu'):
+        self.device = device
+        if device.lower() == "cpu":
+            state = load(weight_path, map_location=torch.device('cpu'))
+        else:
+            state = load(weight_path)
         self.model = eval(state['model'])
         self.model.load_state_dict(state['model_state_dict'])
         self.model.eval()
+        
     def __call__(self, images):
         return self.predict(images)
 
-    def predict(self, images, device = 'cuda:0' if torch.cuda.is_available() else 'cpu'):
-        return predict_images(self.model, images, device)
+    def predict(self, images):
+        return predict_images(self.model, images, self.device)
