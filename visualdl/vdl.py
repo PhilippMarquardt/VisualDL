5614da7d025560ea217c402270f178983c1f39f2
PhilippMarquardt <p.marquardt15@googlemail.com>
2021-08-31 17:05:45 +0200

    .

--- visualdl/vdl.py
+++ visualdl/vdl.py
@@ -1,14 +1,11 @@
-from .utils.utils import parse_yaml
-from .trainer.segmentation.segmentation_trainer import SegmentationTrainer
-from .trainer.classification.classification_trainer import ClassificationTrainer
-from .models.segmentation_model import SegmentationModel
-from .utils.model_utils import predict_images
-from torch import load
-import torch
-from .inference.inference import ModelInference
-
 
 def train(cfg_path):
+    from .utils.utils import parse_yaml
+    from .trainer.segmentation.segmentation_trainer import SegmentationTrainer
+    from .trainer.classification.classification_trainer import ClassificationTrainer
+    from .models.segmentation_model import SegmentationModel
+    
+    
     type = parse_yaml(cfg_path)['type']
     if type == "classification":
         t = ClassificationTrainer(cfg_path=cfg_path)
@@ -19,6 +16,10 @@ def train(cfg_path):
 
 
 def predict(images, weights, device = 'cuda:0' if torch.cuda.is_available() else 'cpu'):
+    import torch
+    from torch import load
+    from .utils.model_utils import predict_images
+    from .models.segmentation_model import SegmentationModel
     state = load(weights)
     model = SegmentationModel.create_model(state['model'])
     model.load_state_dict(state['model_state_dict'])
@@ -26,6 +27,7 @@ def predict(images, weights, device = 'cuda:0' if torch.cuda.is_available() else
 
 
 def get_inference_model(weights, type = "segmentation"):
+    from .inference.inference import ModelInference
     return ModelInference(weights, type=type)
 
 
