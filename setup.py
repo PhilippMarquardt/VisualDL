7de1946f1f0725f400fadd2cedfa4c3020e4000f
PhilippMarquardt <p.marquardt15@googlemail.com>
2021-12-09 13:14:22 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pathlib
 setup(
   name = 'visualdl',
   packages = find_packages(),
-  version = '0.0.4',
+  version = '0.0.5',
   install_requires=[
 'segmentation_models_pytorch @ git+https://github.com/PhilippMarquardt/segmentation_models.pytorch.git@master'
     ,
