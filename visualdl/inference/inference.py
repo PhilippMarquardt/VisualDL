6dc8f2029b8703a06609dccbb380fe33838cb54d
Valentin Haas <76203519+ValeHaas@users.noreply.github.com>
2022-06-30 11:29:58 +0200

    Revert "IMPROVE: Added Error to wrong model type provided"
    This reverts commit 254168a6abdb8966fb8f397bca97d08fa72e4e18.

--- visualdl/inference/inference.py
+++ visualdl/inference/inference.py
@@ -113,9 +113,6 @@ class ModelInference():
             self.model.load_state_dict(state['model_state_dict'])
             self.model.eval()
 
-        else:
-            raise ValueError(f"Unknown modeltype provided: {type}")
-
     def __call__(self, images):
         '''
         Calls predict of this instance selected model
